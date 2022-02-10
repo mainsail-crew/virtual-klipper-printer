322c77e8a64e7425b528c0cc0511d1b48bede08b
th33xitus <th33xitus@googlemail.com>
2022-02-10 21:31:55 +0100

    feat: add fake list-unit output
    this will trick moonraker into thinking that there are actual klipper and moonraker services available

--- docker/service_control.sh
+++ docker/service_control.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ "$1" = "list-units" ]; then
+        echo "klipper.service     loaded   active   running   Klipper Dummy Service"
+        echo "moonraker.service   loaded   active   running   Moonraker Dummy Service"
+        exit 0
+fi
 
 if [ "$2" = "klipper" ]; then
 	sudo /usr/bin/supervisorctl "$1" simulavr "$2"
