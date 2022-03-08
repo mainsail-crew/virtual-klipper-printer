8049e10089e278570167e1c6262848f2beabdd13
th33xitus <th33xitus@googlemail.com>
2022-03-08 16:25:28 +0100

    fix: create and gain permission for `.moonraker_database` (#7)

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,7 @@ cd ~ || exit 1
 [ ! -d ~/klipper_config ] && mkdir klipper_config
 [ ! -d ~/klipper_logs ] && mkdir klipper_logs
 [ ! -d ~/gcode_files ] && mkdir gcode_files
+[ ! -d ~/.moonraker_database ] && mkdir .moonraker_database
 
 [ ! -f ~/klipper_config/printer.cfg ] && \
 cp ~/example-configs/printer.cfg ~/klipper_config/printer.cfg
@@ -14,6 +15,7 @@ cp ~/example-configs/moonraker.conf ~/klipper_config/moonraker.conf
 sudo chown -R printer:printer ~/klipper_config
 sudo chown -R printer:printer ~/klipper_logs
 sudo chown -R printer:printer ~/gcode_files
+sudo chown -R printer:printer ~/.moonraker_database
 
 sudo -S rm /bin/systemctl
 sudo -S ln -s /bin/service_control /bin/systemctl
