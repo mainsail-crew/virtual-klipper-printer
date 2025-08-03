b299c502a21283a84385ebe7d7aa575f633cea5e
Stefan Dej <meteyou@gmail.com>
2025-08-03 16:00:56 +0200

    fix: fix klipper .config file for simulate atmega644p (#50)
    * fix: fix klipper .config file for simulate atmega644p
    
    * fix: fix klipper .config file for simulate atmega644p
    
    * feat: add tmpfs for /tmp and ~/printer_data/comms
    
    * fix: reduce MCU clock and disable not used MCU features
    
    * fix: fix mcu clock in simulavr
    
    * fix: set virtual mcu clock to 20mhz

--- scripts/fix_venvs.sh
+++ scripts/fix_venvs.sh
@@ -3,13 +3,12 @@
 # This file is derived from:
 # https://github.com/mainsail-crew/MainsailOS/blob/develop/modules/raspberry/files/postrename
 
+# delete pycache (*.pyc files)
+find "/home/printer/" -name "__pycache__" -type d -exec rm -rf {} +
+
 venvs=(klippy-env moonraker-env)
 
 for venv in "${venvs[@]}"; do
-    # Move venv
-    # delete pycache (*.pyc files)
-    find "/home/printer/${venv}" -name "__pycache__" -type d -exec rm -rf {} +
-
     # repair shebangs
     while IFS= read -r file ; do
         sed -i 's|/build/'"${venv}"'|/home/printer/'"${venv}"'|g' "${file}"
